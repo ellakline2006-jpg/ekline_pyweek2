714a1535444c837852a74a66a2205735857ba0a4
ellakline2006-jpg <ellakline2006@yahoo.com>
2025-11-12 18:59:27 -0500

    Add files via upload

---
+++ ekline_bedtime.py
@@ -0,0 +1,15 @@
+# Bedtime routine program
+
+# Create a list of bedtime tasks
+bedtime_tasks = [
+    "Brush teeth",
+    "Shower",
+    "Turn off lights",
+    "Get in bed",
+    "Plug in phone"
+]
+
+# Print the list of tasks
+print("🛏️ Bedtime Routine:")
+for i, task in enumerate(bedtime_tasks, start=1):
+    print(f"{i}. {task}")
